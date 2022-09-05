9be13c635f7355f98023f96f339788ba0982a95a
GabriellaViroli <gvccgabriella@gmail.com>
2022-09-05 19:21:52 -0300

    insere data-house, envelopa itens do forEach em <ul> em object keys, altera target dos eventos para classList

--- src/main.js
+++ src/main.js
@@ -8,10 +8,10 @@ let charactersNames;
 const returnToList = document.getElementById('return-btn');
 const modalContent = document.getElementById('modal-content');
 const charactersPercentage = document.getElementById('characters-percentage');
-const cardInfo = document.getElementById('card-info'); 
+const cardInfo = document.getElementById('card-info');
 const charactersList = document.getElementById('characters-list');
-const modalList = document.getElementById('modal-list'); 
-const welcomeSection = document.getElementById('welcome-section'); 
+const modalList = document.getElementById('modal-list');
+const welcomeSection = document.getElementById('welcome-section');
 const sortElement = document.getElementById('sort');
 
 
@@ -26,25 +26,22 @@ function hideModals() {
 
 function formatList(arrayObjects) {
     let list = arrayObjects.map(names => `<li class="list-elements">${names}</li>`).join('');
-    list = "<ul>" + list + "</ul>"
-    return list
+    list = "<ul>" + list + "</ul>";
+    return list;
 }
 
-
-function makeCardInfo(filterData, cardContent, cardTitle) {
-    filterData.map((data) => {
-        let dataInfos = '';
-        Object.keys(data).forEach(key => {
-            data[key] = (data[key] ? data[key] : 'Unknown')
-            if (key != 'id') {
-                dataInfos += `<li class="card-line"><em class="card-content-heading">${key}:</em> ${data[key]}</li>`;
-            }
-
-        })
-        cardContent.innerHTML = dataInfos;
-        cardTitle.innerHTML = `${data.name}`
+function makeCardInfo(data, cardContent, cardTitle) {
+    let dataInfos = '';
+    Object.keys(data).forEach(key => {
+        data[key] = (data[key] ? data[key] : 'Unknown');
+        if (key != 'id') {
+            dataInfos += `<li class="card-line"><em class="card-content-heading">${key}:</em> ${data[key]}</li>`;
+        }
 
     })
+    cardContent.innerHTML = "<ul>" + dataInfos + "</ul>";
+    cardTitle.innerHTML = `${data.name}`;
+
 }
 
 function displayCharacters() {
@@ -63,15 +60,15 @@ btnCharacters.addEventListener('click', displayCharacters);
 
 sortElement.addEventListener('change', () => {
     const sortBy = sortElement.value;
-    charactersNames = mainFilters.sortBy(charactersNames, sortBy)
+    charactersNames = mainFilters.sortBy(charactersNames, sortBy);
     const charactersResult = document.getElementById('characters-content');
     return charactersResult.innerHTML = formatList(charactersNames);
 });
 
 const housesGroupBtn = document.getElementById('houses-group-btn');
 housesGroupBtn.addEventListener('click', listenedElement => {
-    if (listenedElement.target.tagName == 'BUTTON') {
-        const house = listenedElement.target.innerText;
+    if (listenedElement.target.classList.contains('modal-btn')) {
+        const house = listenedElement.target.dataset.house;
         charactersNames = mainFilters.filterCharactersByHouses(dataBaseCharacters, house);
         const sortBy = sortElement.value;
         charactersNames = mainFilters.sortBy(charactersNames, sortBy);
@@ -81,7 +78,7 @@ housesGroupBtn.addEventListener('click', listenedElement => {
         charactersList.style.display = 'block';
         charactersPercentage.style.display = 'block';
         document.getElementById('header-modal-characters').innerText = `Characters from house ${house}`;
-        charactersPercentage.innerHTML = `The characters in <em class="house-name">${house}</em> represent ${percentageOfCharsByHouse}% of all characters in the Harry Potter Books`
+        charactersPercentage.innerHTML = `The characters in <em class="house-name">${house}</em> represent ${percentageOfCharsByHouse}% of all characters in the Harry Potter Books`;
         const charactersByHouseResult = document.getElementById('characters-content');
         return charactersByHouseResult.innerHTML = formatList(charactersNames);
     }
@@ -89,7 +86,7 @@ housesGroupBtn.addEventListener('click', listenedElement => {
 
 const charactersContent = document.getElementById('characters-content');
 charactersContent.addEventListener('click', (listedCharacter) => {
-    if (listedCharacter.target.tagName == 'LI') {
+    if (listedCharacter.target.classList.contains('list-elements')) {
         returnToList.removeEventListener('click', displayCharacters);
         returnToList.addEventListener('click', displayCharacters);
         charactersList.style.display = 'none';
@@ -99,7 +96,7 @@ charactersContent.addEventListener('click', (listedCharacter) => {
         const clickedName = listedCharacter.target.innerText;
         const filterCharacters = dataBaseCharacters.filter((character) => character.name === clickedName);
 
-        makeCardInfo(filterCharacters, cardContent, cardTitle)
+        makeCardInfo(filterCharacters[0], cardContent, cardTitle);
     }
 
 });
@@ -111,11 +108,10 @@ function displayBooksList() {
     document.getElementById('modal-header-title').innerText = 'Books list';
 
     modalContent.innerHTML = formatList(bookTitles);
-
     modalContent.addEventListener('click', listenedElement => {
-        if (listenedElement.target.tagName == 'LI') {
+        if (listenedElement.target.classList.contains('list-elements')) {
             let bookTitle = listenedElement.target.innerText;
-            displayBookCard(bookTitle)
+            displayBookCard(bookTitle);
         }
     });
 }
@@ -133,10 +129,10 @@ function displayBookCard(bookTitle) {
     const filterBooks = dataBaseBooks.filter((book) => book.title === bookTitle);
 
     return filterBooks.map((book) => {
-        if (book.releaseDay == null) book.releaseDay = 'Unknown'
-        if (book.author == null) book.author = 'Unknown'
-        if (book.description == null) book.description = 'Unknown'
-        cardTitle.innerHTML = `${book.title}`
+        if (book.releaseDay == null) book.releaseDay = 'Unknown';
+        if (book.author == null) book.author = 'Unknown';
+        if (book.description == null) book.description = 'Unknown';
+        cardTitle.innerHTML = `${book.title}`;
         cardContent.innerHTML =
             `
             <li class="card-line"><em class="card-content-heading">Release Date:</em> ${book.releaseDay}</li>
@@ -148,29 +144,29 @@ function displayBookCard(bookTitle) {
 
 function displaySpellsList() {
     const spellsNames = mainFilters.getSpellsNames(dataBaseSpells);
-    hideModals()
+    hideModals();
     modalList.style.display = 'block';
     document.getElementById('modal-header-title').innerText = 'Spells list';
 
     modalContent.innerHTML = formatList(spellsNames);
 
     modalContent.addEventListener('click', listenedElement => {
-        if (listenedElement.target.tagName == 'LI') {
+        if (listenedElement.target.classList.contains('list-elements')) {
             let spellName = listenedElement.target.innerText;
             returnToList.removeEventListener('click', displaySpellsList);
             returnToList.addEventListener('click', displaySpellsList);
             modalList.style.display = 'none';
             cardInfo.style.display = 'block';
-            const cardContent = document.getElementById('card-content-info')
-            const cardTitle = document.getElementById('card-info-title')
+            const cardContent = document.getElementById('card-content-info');
+            const cardTitle = document.getElementById('card-info-title');
             const filterSpells = dataBaseSpells.filter((spell) => spell.name === spellName);
 
             return filterSpells.map((spell) => {
-                if (spell.pronunciation == null) spell.pronunciation = 'Unknown'
-                if (spell.spell_type == null) spell.spell_type = 'Unknown'
-                if (spell.description == null) spell.description = 'Unknown'
-                if (spell.mention == null) spell.mention = 'Unknown'
-                cardTitle.innerHTML = `${spell.name}`
+                if (spell.pronunciation == null) spell.pronunciation = 'Unknown';
+                if (spell.spell_type == null) spell.spell_type = 'Unknown';
+                if (spell.description == null) spell.description = 'Unknown';
+                if (spell.mention == null) spell.mention = 'Unknown';
+                cardTitle.innerHTML = `${spell.name}`;
                 cardContent.innerHTML =
                     `
             <li class="card-line"><em class="card-content-heading">Pronunciation:</em> ${spell.pronunciation}</li>
